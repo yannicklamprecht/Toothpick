96ebe8b31f1fcb8af5f548c0164c2db8796b6018
ysl3000 <yannicklamprecht@live.de>
2020-08-14 17:27:51 +0200

    add gitlab ci file

--- subprojects/fake.gradle.kts
+++ subprojects/fake.gradle.kts
@@ -4,8 +4,20 @@ plugins {
 
 // This project is used to resolve dependencies, used by mercury in the mapping process
 
+val parentPomFile = project.parent?.projectDir?.resolve("work/Paper/pom.xml")
 val serverPomFile = project.parent?.projectDir?.resolve("work/Paper/Paper-Server/pom.xml")
 val apiPomFile = project.parent?.projectDir?.resolve("work/Paper/Paper-API/pom.xml")
+
+if (parentPomFile != null) {
+    repositories {
+        loadRepositories(parentPomFile, project)
+    }
+
+    dependencies {
+        loadDependencies(parentPomFile, project, true)
+    }
+}
+
 if (serverPomFile != null) {
     repositories {
         loadRepositories(serverPomFile, project)
